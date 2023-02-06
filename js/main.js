61057879f20f82f70b8e5df00384accaebf8053a
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-02-06 14:59:47 +0100

    + MILESTONE 0

---
+++ js/main.js
@@ -0,0 +1,63 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+
+// MILESTONE 1: Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//
+// MILESTONE 2: tampare le stesse informazioni su DOM sottoforma di stringhe
+
+// BONUS 1: Trasformare la stringa foto in una immagine effettiva
+//
+// BONUS 2: Organizzare i singoli membri in card/schede
+
+// MILESTONE 0
+
+const ourTeam = [
+  {
+    name: "Wayne",
+    lastname: "Barnett",
+    position: "Founder & CEO",
+    photo: "./img/wayne-barnett-founder-ceo.jpg",
+  },
+
+  {
+    name: "Angela",
+    lastname: "Caroll",
+    position: "Chief Editor",
+    photo: "./img/angela-caroll-chief-editor.jpg",
+  },
+
+  {
+    name: "Walter",
+    lastname: "Gordon",
+    position: "Office Manager",
+    photo: "./img/walter-gordon-office-manager.jpg",
+  },
+
+  {
+    name: "Angela",
+    lastname: "Lopez",
+    position: "Social Media Manager	",
+    photo: "./img/angela-lopez-social-media-manager.jpg",
+  },
+
+  {
+    name: "Scott",
+    lastname: "Estrada",
+    position: "Developer",
+    photo: "./img/scott-estrada-developer.jpg",
+  },
+
+  {
+    name: "Barbara",
+    lastname: "Ramos",
+    position: "Graphic Designer",
+    photo: "./img/barbara-ramos-graphic-designer.jpg",
+  },
+];
+console.log(ourTeam);
+console.log(ourTeam[1]);
+console.log(ourTeam[1].name);
+
+// MILESTONE 1
